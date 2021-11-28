fe4145720ba681094f10b0ad0cd25c2783f95ced
xandowski <xandy-moraes@hotmail.com>
2021-11-27 21:48:22 -0300

    notebooks

---
+++ olist/src/upload_data.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+files_names = [i for i in os.listdir(DATA_DIR) if i.endswith('.csv')]
+
+for i in files_names:
+    df_tmp = pd.read_csv(os.path.join(DATA_DIR, i))
